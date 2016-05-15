c2bb12eb7055751d505c57dd14bdff78b8b1d030
taca <taca>
2016-05-15 14:15:14 +0000

    Update ruby21 and ruby21-base package to 2.1.10.
    Ruby 2.1.10 Released				Posted by usa on 1 Apr 2016
    
    Ruby 2.1.10 has been released. This release is not intended for production
    use, but for compatibility tests with two-digit version numbers. You don¡Çt
    have to replace Ruby 2.1.9 by 2.1.10 in normal use.
    
    As announced in the 2.1.9 release post, Ruby 2.1.10 does not include any
    changes from 2.1.9, except for its version number (and only one small related
    change in its test suite). Please test your applications and/or libraries for
    compatibility with two-digit version numbers.
    
    Ruby 2.1.9 Released				Posted by usa on 30 Mar 2016
    
    Ruby 2.1.9 has been released.
    
    This release includes many bug fixes. See ChangeLog for details.
    
    As announced before, this is the last normal release of the Ruby 2.1
    series. After this release we will never backport any bug fixes to 2.1 except
    security fixes. We recommend that you start planning to upgrade to Ruby 2.3 or
    2.2.
    
    By the way, we are planning to release Ruby 2.1.10 in a few days. Ruby 2.1.10
    will not include any changes from 2.1.9, except for its version number. You do
    not have to use it on production, but you should test it because it has a
    two-digit version number.

--- lang/ruby/rubyversion.mk
+++ lang/ruby/rubyversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: rubyversion.mk,v 1.161 2016/05/15 14:07:48 taca Exp $
+# $NetBSD: rubyversion.mk,v 1.162 2016/05/15 14:15:14 taca Exp $
 #
 
 # This file determines which Ruby version is used as a dependency for
@@ -228,13 +228,13 @@ RUBY_VERSION_REQD?= ${PKGNAME_REQD:C/ruby([0-9][0-9]+)-.*/\1/}
 # current supported Ruby's version
 RUBY18_VERSION=		1.8.7
 RUBY200_VERSION=	2.0.0
-RUBY21_VERSION=		2.1.8
+RUBY21_VERSION=		2.1.10
 RUBY22_VERSION=		2.2.5
 
 # patch
 RUBY18_PATCHLEVEL=	pl374
 RUBY200_PATCHLEVEL=	p648
-#RUBY21_PATCHLEVEL=	p440
+#RUBY21_PATCHLEVEL=	p492
 #RUBY22_PATCHLEVEL=	p319
 
 # current API compatible version; used for version of shared library
